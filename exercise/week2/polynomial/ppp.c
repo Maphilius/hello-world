7a30def485ac896ab02514c539e45be6f4491842
fish98 <jiongchiyu@gmail.com>
2018-03-24 18:52:30 +0800

    ready to fix buddle bugs

--- exercise/week2/polynomial/ppp.c
+++ exercise/week2/polynomial/ppp.c
@@ -16,12 +16,12 @@ typedef struct list {
 
 /* 函数声明 */
 
-void push(List* tmpList, int co, int ex);  // 添加链表节点 
-void print(List *tmpList);                 // 打印链表 
-void operate(List* list1, List* list2);      // 对多项式进行操作 
-void Add(List* list1, List* list2);
-void Minus(List* list1, List* list2);
-void Multiply(List* list1, List* list2);
+void push(List* tmpList, int co, int ex);    // 添加链表节点 
+void print(List *tmpList);                   // 打印链表 
+void operate(List* 1list, List* 2list);      // 对多项式进行操作 
+void Add(List* 1list, List* 2list);
+void Minus(List* 1list, List* 2list);
+void Multiply(List* 1list, List* 2list);
 
 /* 将输入的项传入链表 */ 
 
@@ -39,14 +39,14 @@ void push(List* tmpList, int co, int ex)
 	}
 }
 
-/* 将得到的多项式链表打印*/ 
+/* 将得到的多项式链表打印 */ 
 
 void print(List *tmpList){
 	Node* p;
 	printf("("); 
 	for( p = tmpList->head; p; p = p->next){
-		if( p->next == NULL){  // print前检测最后一项 
-			if(p->exp == 0){  // 如果是常数项
+		if( p->next == NULL){           // print前检测最后一项 
+			if(p->exp == 0){            // 如果是常数项
 				printf("%d", p->coef);  // print 常数项 
 			}
 			else{
@@ -62,9 +62,9 @@ void print(List *tmpList){
 
 /* 获取操作运算符并操作 */
 
-void operate(List* list1, List* list2){ 
+void operate(List* 1list, List* 2list){ 
 	printf("请输入需要操作的指令： (+ / - / * )\n");
-	char op;          // operator 居然是一个保留词！！！  
+	char op;             // operator 居然是一个保留词！！！  
 	scanf("%c", &op);
 	while (op == 10) {
 		scanf("%c", &op);
@@ -74,30 +74,68 @@ void operate(List* list1, List* list2){
 		case '-': Minus(list1, list2); break;
 		case '*': Multiply(list1, list2); break;
 		default : printf("请输入合法的操作符\n"); operate(list1, list2);
-  }
+    }
 } 
 
 /* 加法处理 */
 
-void Add(List* list1, List* list2){
+void Add(List* 1list, List* 2list){
+//	Node * a, *b, *c;
+//	Node *f,*r,*t;  
+//    int sum;  
+//    r=(Node *)malloc(sizeof(Node));  
+//    f=r;  
+//    while(list1&&list2)  
+//    {  
+//        switch(compare(pList->v.z,qList->v.z))  
+//        {  
+//            case 1:  
+//                accach(pList->v.x,pList->v.z,&r);  
+//                pList=pList->next;  
+//                break;  
+//            case -1:  
+//                accach(qList->v.x,qList->v.z,&r);  
+//                qList=qList->next;  
+//                break;  
+//            case 0:  
+//                sum = pList->v.x+qList->v.x;  
+//                if(sum)accach(sum,pList->v.z,&r);  
+//                pList=pList->next;  
+//                qList=qList->next;  
+//                break;  
+//        }  
+//    }  
+//     for(;pList;pList=pList->next)accach(pList->v.x,pList->v.z,&r);  
+//    for(;qList;qList=qList->next)accach(qList->v.x,qList->v.z,&r);  
+//    r->next=NULL;  
+//    t=f;  
+//    f=f->next;  
+//    free(t);  
+//    return f;  
+//  
 	
 } 
 
 /* 减法处理 */
 
-void Minus(List* list1, List* list2){
+void Minus(List* 1list, List* 2list){
 	
 } 
 
 /* 乘法处理 */
 
-void Multiply(List* list1, List* list2){
+void Multiply(List* 1list, List* 2list){
 	
 } 
 
+void Sort(List* list){
+	Node * point;
+	
+}
+
 int main()
 {       
-	List list1, list2, outlist;  // 定义三个链表 
+	List list1, list2;      // 定义三个链表 
 	int n, i;               // for循环使用的操作变量 
 	int co, exp;            // 定义多项式的单个项的系数与指数 
 	
@@ -109,19 +147,19 @@ int main()
 	
 	/* 获取多项式 1 */ 
 	
-	printf("多项式1的项数： \n");  // 首先获取多项式项数 
-	scanf("%d",&n);
+	printf("多项式1的项数： ");    // 首先获取多项式项数 
+	scanf("%d", &n);
 	printf("输入多项式1： ");      // 依次输入各项 
 	for(i = 0; i < n; i++){
 		scanf("%d %d", &co, &exp);
 	    push(&list1, co, exp);
 		}
 	print(&list1);
-	printf("\n");  // 不优雅的手动换行 
+	printf("\n");           // 不优雅的手动换行 
 	
 	/* 获取多项式 2 */ 
 		
-	printf("多项式2的项数： \n");
+	printf("多项式2的项数： ");
 	scanf("%d",&n);
 	printf("输入多项式2： ");
 	for(i = 0; i < n; i++){
@@ -129,9 +167,10 @@ int main()
 		push(&list2, co, exp);
 		}
 	print(&list2);
-	printf("\n");  //不优雅的手动换行 
+	printf("\n");           //不优雅的手动换行 
 	
 	/* 获取操作符号 */ 
+	
 	operate(&list1, &list2);
 	
 	return 0;
