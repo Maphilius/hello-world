3fee82a4aa082ee9542d97b3933d3f574cc5614d
Maphilius <Maphilius@Outlook.com>
2018-05-14 22:26:04 +0800

    ok

--- exercise/week5/Recursion/Recursion.c
+++ exercise/week5/Recursion/Recursion.c
@@ -29,17 +29,75 @@ void Listmnemonics(char* str)
 			printf("\n");
 		}	
 	}
-	if ("0"<=str[2]<="1"&&"2"<=str[1]<="9"&&"2"<=str[0]<="9"||
-		"2"<=str[2]<="9"&&"0"<=str[1]<="1"&&"2"<=str[0]<="9"||
-		"2"<=str[2]<="9"&&"2"<=str[1]<="9"&&"0"<=str[0]<="1"){
+	if ("0"<=str[2]<="1"&&"2"<=str[1]<="9"&&"2"<=str[0]<="9"){
 		for (i = 0;i < 3;i++){
 			for (j = 0;j < 3;j++){
-				for (k = 0;k < 3;k++){
-					printf("%c%c%c ",change(str[0])+i,change(str[1])+j,change(str[2])+k);
-				}
+				
+				printf("%c%c%c ",change(str[0])+i,change(str[1])+j,change(str[2]));
+				
 			}
 			printf("\n");
 		}	
+	}
+	if ("2"<=str[2]<="9"&&"0"<=str[1]<="1"&&"2"<=str[0]<="9"){
+		for (i = 0;i < 3;i++){
+			for (k = 0;k < 3;k++){
+				
+				printf("%c%c%c ",change(str[0])+i,change(str[1]),change(str[2])+k);
+				
+			}
+			printf("\n");
+		}	
+	}
+		if ("2"<=str[2]<="9"&&"2"<=str[1]<="9"&&"0"<=str[0]<="1"){
+		for (j = 0;j < 3;j++){
+			for (k = 0;k < 3;k++){
+				
+					printf("%c%c%c ",change(str[0]),change(str[1])+j,change(str[2])+k);
+				
+			}
+			printf("\n");
+		}	
+	}
+		if ("0"<=str[2]<="1"&&"0"<=str[1]<="1"&&"2"<=str[0]<="9"){
+		for (i = 0;i < 3;i++){
+			
+				
+				printf("%c%c%c ",change(str[0])+i,change(str[1]),change(str[2]));
+				
+			
+			printf("\n");
+		}	
+	}
+	if ("2"<=str[2]<="9"&&"0"<=str[1]<="1"&&"0"<=str[0]<="1"){
+		for (k = 0;k < 3;k++){
+			
+				
+				printf("%c%c%c ",change(str[0]),change(str[1]),change(str[2])+k);
+				
+			
+			printf("\n");
+		}	
+	}
+		if ("0"<=str[2]<="1"&&"2"<=str[1]<="9"&&"0"<=str[0]<="1"){
+		for (j = 0;j < 3;j++){
+			
+				
+					printf("%c%c%c ",change(str[0]),change(str[1])+j,change(str[2]));
+				
+			
+			printf("\n");
+		}	
+	}
+	if ("0"<=str[2]<="1"&&"0"<=str[1]<="1"&&"0"<=str[0]<="1"){
+		
+			
+				
+					printf("%c%c%c ",change(str[0]),change(str[1]),change(str[2]));
+				
+			
+			printf("\n");
+			
 	}
 }
 
